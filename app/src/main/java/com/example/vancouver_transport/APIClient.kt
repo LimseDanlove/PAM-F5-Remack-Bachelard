2d5e3ff5b41bb9404815469d927bd4d0ba96251b
Justine BACHELARD <justine.bachelard@be-ys.cloud>
2022-01-17 08:17:55 +0100

    :sparkles: parsing search results

--- app/src/main/java/com/example/vancouver_transport/APIClient.kt
+++ app/src/main/java/com/example/vancouver_transport/APIClient.kt
@@ -11,6 +11,7 @@ import io.ktor.http.*
 import kotlinx.serialization.builtins.serializer
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.jsonArray
 import kotlinx.serialization.json.jsonObject
 import org.json.JSONArray
 import org.json.JSONObject
@@ -41,19 +42,22 @@ class APIClient(cio : CIO) {
     }
 
     suspend fun getSH(name: String): ArrayList<SuperHero>{
+        // maybe put API token as attribute ?
         val httpResponse: HttpResponse = client.get("https://www.superheroapi.com/api.php/1106077463536216/search/$name")
         val listSH = ArrayList<SuperHero>()
 
         if(httpResponse.status == HttpStatusCode.OK) {
-            //val jsonBody = JSONObject(httpResponse.receive<String>())
-            //val results = JSONArray(jsonBody.get("results"))
+            // get body as JsonElement object
             val jsonBody : JsonElement = Json.decodeFromString(JsonElement.serializer(),httpResponse.receive())
 
-            Log.println(Log.INFO,"TEST",jsonBody.toString())
+            // get "results" JsonElement
             val results = Json.decodeFromString(JsonElement.serializer(),jsonBody.jsonObject["results"].toString())
 
-            //results.jsonObject.forEach()
-
+            // parse each result from array results
+            for (i in 0 until results.jsonArray.size) {
+                // right place to parse ?
+                listSH.add(ParseSuperHero.fromJson(results.jsonArray[i].toString()))
+            }
         }
         return listSH
     }
